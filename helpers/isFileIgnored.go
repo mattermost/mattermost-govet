4bff951f81ead6ce0eeb1dc69a008a5b757c94cb
Jesús Espino <jespinog@gmail.com>
2024-12-04 14:00:12 +0100

    feat: Add ignore flag to emptyInterface analyzer (#41)
    * feat: Add ignore flag to emptyInterface analyzer
    
    * feat: Add isIgnore helper function for file/path filtering
    
    * refactor: Move isIgnore function to helpers package
    
    * Fixing imports
    
    * refactor: Rename IsIgnore function to IsFileIgnored
    
    * Moving isIngore file

---
+++ helpers/isFileIgnored.go
@@ -0,0 +1,15 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+package helpers
+
+import "strings"
+
+func IsFileIgnored(file string, ignoreFiles []string) bool {
+	for _, f := range ignoreFiles {
+		if strings.HasSuffix(file, f) {
+			return true
+		}
+	}
+	return false
+}
